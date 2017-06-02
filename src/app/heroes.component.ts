b9b3ea6f51764ff83f47b3db667346cec6dea617
Nelson Toro <nelson.toro@jalasoft.com>
2017-06-02 13:44:46 -0400

    Completed the Angular tutorial, exists an issue when you remove any hero the dashboard won't be updated

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-heroes',
+  styleUrls: ['./heroes.component.css'],
+  templateUrl: './heroes.component.html'
+})
+
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService, private router: Router ) {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  };
+  getHeroes(): void {
+    this.heroService.getHeroesFromServer().then(heroes => this.heroes = heroes);
+  };
+  ngOnInit(): void {
+    this.getHeroes();
+  };
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) {
+            this.selectedHero = null;
+          }
+        });
+  }
+}
